65169e5f37d35f8a4dbfe54435bcb50e7276af30
Vgharshak <84949833+VagharshakGrigoryan@users.noreply.github.com>
2022-05-04 22:42:13 +0400

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText user_field;
+    private TextView result_info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user_field = findViewById(R.id.user_field);
+        Button main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
+
+//        main_btn.setOnClickListener(new View.OnClickListener() {
+        main_btn.setOnClickListener(view -> {
+            if (user_field.getText().toString().trim().equals(""))
+                Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
+            else {
+                String city = user_field.getText().toString();
+                String key = "bf6d93712a56e68d974a02c109b665ed";
+                String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric&lang=ru\n";
+
+                new GetUrlDat().execute(url);
+            }
+        });
+    }
+
+    private class GetUrlDat extends AsyncTask<String, String, String> {
+
+        private void orPreExecute() {
+            super.onPreExecute();
+            result_info.setText("expect...");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+
+        }
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute (String result){
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject =  new JSONObject(result);
+                result_info.setText("temperature " + jsonObject.getJSONObject("main").getDouble("temp"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
